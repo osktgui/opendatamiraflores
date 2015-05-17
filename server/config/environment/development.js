194faace7ee3c547ad54b72df4fdc8c64f8fcd9b
luisoscategui <luis.oscategui@xurface.com>
2015-05-17 00:34:44 -0500

    project open data

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/opendatamiraflores-dev'
+  },
+
+  seedDB: true
+};
